45706479ec62327e6ee6596adad58c90288d27f2
ittsel-ali <ittsel1@gmail.com>
2020-06-15 19:51:22 -0400

    Update rush_test.rb
    Implemented all test cases for sort and filter features

--- test/models/rush_test.rb
+++ test/models/rush_test.rb
@@ -9,8 +9,9 @@ class RushTest < ActiveSupport::TestCase
         Rails.root.join('test/fixtures/files/rushing.json') 
         ) 
       )
-
-    assert_equal Rush.load_json, expected
+    
+    rush = Rush.new
+    assert_equal rush.records, expected
   end
 
   test "sort method with no args: should return the same set of records as fixture" do
@@ -20,54 +21,106 @@ class RushTest < ActiveSupport::TestCase
         Rails.root.join('test/fixtures/files/rushing.json') 
         ) 
       )
-
-    assert_equal Rush.sort, expected
+    
+    rush = Rush.new
+    assert_equal rush.sort, expected
   end
 
-  test "sort method with Longest Rush option: should return two desc sorted records with Lng <= -4" do
+  test "Search Player by name: should return three unsorted records" do
     expected = 
     [
       {
-        "Player"=>"Travis Kelce", "Team"=>"KC", "Pos"=>"TE", "Att"=>1, "Att/G"=>0.1, "Yds"=>-5, "Avg"=>-5, 
-        "Yds/G"=>-0.3, "TD"=>0, "Lng"=>-5, "1st"=>0, "1st%"=>0, "20+"=>0, "40+"=>0, "FUM"=>0
+        "Player"=>"Travis Benjamin", "Team"=>"SD", "Pos"=>"WR", "Att"=>2, "Att/G"=>0.1, "Yds"=>-3, "Avg"=>-1.5, 
+        "Yds/G"=>-0.2, "TD"=>0, "Lng"=>-1, "1st"=>0, "1st%"=>0, "20+"=>0, "40+"=>0, "FUM"=>0
       },
       {
-        "Player"=>"Taiwan Jones", "Team"=>"OAK", "Pos"=>"RB", "Att"=>1, "Att/G"=>0.1, "Yds"=>-8, "Avg"=>-8, 
-        "Yds/G"=>-0.6, "TD"=>0, "Lng"=>-8, "1st"=>0, "1st%"=>0, "20+"=>0, "40+"=>0, "FUM"=>0
+        "Player"=>"Travis Kelce", "Team"=>"KC", "Pos"=>"TE", "Att"=>1, "Att/G"=>0.1, "Yds"=>-5, 
+        "Avg"=>-5, "Yds/G"=>-0.3, "TD"=>0, "Lng"=>-5, "1st"=>0, "1st%"=>0, "20+"=>0, "40+"=>0, "FUM"=>0
+      }, 
+      {
+        "Player"=>"Travaris Cadet", "Team"=>"NO", "Pos"=>"RB", "Att"=>4, "Att/G"=>0.3, "Yds"=>19, "Avg"=>4.8, 
+        "Yds/G"=>1.3, "TD"=>0, "Lng"=>"16", "1st"=>1, "1st%"=>25, "20+"=>0, "40+"=>0, "FUM"=>0
       }
     ]
-    
-    assert_equal Rush.sort(options: {"Lng"=>"-4"}), expected
+    rush = Rush.new
+    rush.filter(field: "Player", search: "tra")
+
+    assert_equal rush.records, expected
   end
 
-  test "sort method with Longest Rush and Total Rushing Yards option: should return one desc sorted records with Lng <= -4 and Yds <= -8" do
+  test "Ascending sort method with Longest Rush option: should return three sorted records by Lng field" do
     expected = 
     [
       {
-        "Player"=>"Taiwan Jones", "Team"=>"OAK", "Pos"=>"RB", "Att"=>1, "Att/G"=>0.1, "Yds"=>-8, "Avg"=>-8, 
-        "Yds/G"=>-0.6, "TD"=>0, "Lng"=>-8, "1st"=>0, "1st%"=>0, "20+"=>0, "40+"=>0, "FUM"=>0
+        "Player"=>"Travis Kelce", "Team"=>"KC", "Pos"=>"TE", "Att"=>1, "Att/G"=>0.1, "Yds"=>-5, 
+        "Avg"=>-5, "Yds/G"=>-0.3, "TD"=>0, "Lng"=>-5, "1st"=>0, "1st%"=>0, "20+"=>0, "40+"=>0, "FUM"=>0
+      }, 
+      {
+        "Player"=>"Travis Benjamin", "Team"=>"SD", "Pos"=>"WR", "Att"=>2, "Att/G"=>0.1, "Yds"=>-3, "Avg"=>-1.5, 
+        "Yds/G"=>-0.2, "TD"=>0, "Lng"=>-1, "1st"=>0, "1st%"=>0, "20+"=>0, "40+"=>0, "FUM"=>0
+      }, 
+      {
+        "Player"=>"Travaris Cadet", "Team"=>"NO", "Pos"=>"RB", "Att"=>4, "Att/G"=>0.3, "Yds"=>19, "Avg"=>4.8, 
+        "Yds/G"=>1.3, "TD"=>0, "Lng"=>"16", "1st"=>1, "1st%"=>25, "20+"=>0, "40+"=>0, "FUM"=>0
       }
     ]
-    
-    assert_equal Rush.sort(options: {"Lng"=>"-4", "Yds"=>"-8"}), expected
+    rush = Rush.new
+    rush.filter(field: "Player", search: "tra")
+
+    assert_equal rush.sort(field: "Lng"), expected
   end
 
-  test "sort method with Longest Rush option and offset/limit: should return sorted records with offset and limit" do
+  
+  test "sort Longest Rush with offset/limit option: should return sorted records with offset and limit" do
     expected = 
     [
       {
-        "Player"=>"Travis Kelce", "Team"=>"KC", "Pos"=>"TE", "Att"=>1, "Att/G"=>0.1, "Yds"=>-5, "Avg"=>-5, 
-        "Yds/G"=>-0.3, "TD"=>0, "Lng"=>-5, "1st"=>0, "1st%"=>0, "20+"=>0, "40+"=>0, "FUM"=>0
-      },
+        "Player"=>"Travis Kelce", "Team"=>"KC", "Pos"=>"TE", "Att"=>1, "Att/G"=>0.1, "Yds"=>-5, 
+        "Avg"=>-5, "Yds/G"=>-0.3, "TD"=>0, "Lng"=>-5, "1st"=>0, "1st%"=>0, "20+"=>0, "40+"=>0, "FUM"=>0
+      }, 
       {
-        "Player"=>"Taiwan Jones", "Team"=>"OAK", "Pos"=>"RB", "Att"=>1, "Att/G"=>0.1, "Yds"=>-8, "Avg"=>-8, 
-        "Yds/G"=>-0.6, "TD"=>0, "Lng"=>-8, "1st"=>0, "1st%"=>0, "20+"=>0, "40+"=>0, "FUM"=>0
+        "Player"=>"Travis Benjamin", "Team"=>"SD", "Pos"=>"WR", "Att"=>2, "Att/G"=>0.1, "Yds"=>-3, "Avg"=>-1.5, 
+        "Yds/G"=>-0.2, "TD"=>0, "Lng"=>-1, "1st"=>0, "1st%"=>0, "20+"=>0, "40+"=>0, "FUM"=>0
+      }, 
+      {
+        "Player"=>"Travaris Cadet", "Team"=>"NO", "Pos"=>"RB", "Att"=>4, "Att/G"=>0.3, "Yds"=>19, "Avg"=>4.8, 
+        "Yds/G"=>1.3, "TD"=>0, "Lng"=>"16", "1st"=>1, "1st%"=>25, "20+"=>0, "40+"=>0, "FUM"=>0
+      }
+    ]
+
+    rush = Rush.new
+    rush.filter(field: "Player", search: "tra")
+    
+    assert_equal rush.sort(field: "Lng", offset: 0, limit: 1), [ expected[0] ] 
+    assert_equal rush.sort(field: "Lng", offset: 1, limit: 1), [ expected[1] ] 
+    assert_equal rush.sort(field: "Lng", offset: 0, limit: 3),   expected 
+  end
+
+  test "Generate CSV of rushing records correctly" do
+    records = 
+    [
+      {
+        "Player"=>"Travis Kelce", "Team"=>"KC", "Pos"=>"TE", "Att"=>1, "Att/G"=>0.1, "Yds"=>-5, 
+        "Avg"=>-5, "Yds/G"=>-0.3, "TD"=>0, "Lng"=>-5, "1st"=>0, "1st%"=>0, "20+"=>0, "40+"=>0, "FUM"=>0
+      }, 
+      {
+        "Player"=>"Travis Benjamin", "Team"=>"SD", "Pos"=>"WR", "Att"=>2, "Att/G"=>0.1, "Yds"=>-3, "Avg"=>-1.5, 
+        "Yds/G"=>-0.2, "TD"=>0, "Lng"=>-1, "1st"=>0, "1st%"=>0, "20+"=>0, "40+"=>0, "FUM"=>0
+      }, 
+      {
+        "Player"=>"Travaris Cadet", "Team"=>"NO", "Pos"=>"RB", "Att"=>4, "Att/G"=>0.3, "Yds"=>19, "Avg"=>4.8, 
+        "Yds/G"=>1.3, "TD"=>0, "Lng"=>"16", "1st"=>1, "1st%"=>25, "20+"=>0, "40+"=>0, "FUM"=>0
       }
     ]
     
-    assert_equal Rush.sort(options: {"Lng"=>"-4"}, offset: 0, limit: 1), [ expected[0] ] 
-    assert_equal Rush.sort(options: {"Lng"=>"-4"}, offset: 1, limit: 1), [ expected[1] ] 
-    assert_equal Rush.sort(options: {"Lng"=>"-4"}, offset: 0, limit: 2),   expected 
+    expected = []
+    expected << "Player,Team,Pos,Att,Att/G,Yds,Avg,Yds/G,TD,Lng,1st,1st%,20+,40+,FUM\n"
+    expected << "Travis Kelce,KC,TE,1,0.1,-5,-5,-0.3,0,-5,0,0,0,0,0\n"
+    expected << "Travis Benjamin,SD,WR,2,0.1,-3,-1.5,-0.2,0,-1,0,0,0,0,0\n"
+    expected << "Travaris Cadet,NO,RB,4,0.3,19,4.8,1.3,0,16,1,25,0,0,0\n"
+    
+    rush = Rush.new
+    assert_equal rush.generate_csv(records), expected.join 
   end
 
   
